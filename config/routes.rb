8242218b1f022645a2bbe859b40b1b0f7442ee20
terra-cloud <maiashea@gmail.com>
2014-07-05 04:23:49 +0000

    I did stuff that does things.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+
   get 'pages/info' => 'pages#info'
   root :to => redirect('/ideas')
   resources :ideas
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140704230023) do
+ActiveRecord::Schema.define(version: 20140705012543) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ideas", force: true do |t|
     t.string   "name"
